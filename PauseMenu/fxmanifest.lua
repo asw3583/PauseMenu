f9b659d0805ebc443b32b09a0b8d007c1c326dbe
asw3583 <115332340+asw3583@users.noreply.github.com>
2023-03-12 21:57:18 +0900

    Add files via upload

---
+++ PauseMenu/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Zoygkos#5869'
+
+
+--Client Scripts-- 
+client_scripts {
+ 'Client/*.lua'
+}
+
+--Server Scripts-- 
+server_scripts {
+ 'Server/*.lua'
+}
+
+
+--UI Part-- 
+ui_page {
+ 'html/index.html', 
+}
+
+--File Part-- 
+files {
+ 'html/index.html',
+ 'html/app.js', 
+ 'html/style.css'
+} 
+client_script "@Badger-Anticheat-master/acloader.lua"
